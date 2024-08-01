9de52c574f960166fae14895653f4e4cefe4cac6
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-01 08:03:00 +0200

    Created db

---
+++ OutOfOfficeEF/OutOfOfficeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOfficeEF
+{
+    public class OutOfOfficeContext: DbContext
+    {
+        public string DbPath { get; }
+        public OutOfOfficeContext() {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "out_of_office_app.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
