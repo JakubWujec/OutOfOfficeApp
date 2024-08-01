76910ccb3e5d5e314539a0e29a370ce6cb758f21
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-01 14:43:53 +0200

    starting app from App.onstartup

--- OutOfOfficeWPF/App.xaml.cs
+++ OutOfOfficeWPF/App.xaml.cs
@@ -9,6 +9,14 @@ namespace OutOfOfficeWPF
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();      
+        }
     }
 
 }
