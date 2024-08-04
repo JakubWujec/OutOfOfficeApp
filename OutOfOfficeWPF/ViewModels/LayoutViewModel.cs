915ec362cd06daacc1ac55af16be611a4dc50246
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-04 20:34:43 +0200

    LayoutViewModel

---
+++ OutOfOfficeWPF/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOfficeWPF.ViewModels
+{
+    public class LayoutViewModel: ViewModelBase
+    {
+        public NavigationBarViewModel NavigationBarViewModel;
+        public ViewModelBase CurrentViewModel;
+
+        public LayoutViewModel(NavigationBarViewModel navigationBarViewModel, ViewModelBase currentViewModel)
+        {
+            this.NavigationBarViewModel = navigationBarViewModel;
+            this.CurrentViewModel = currentViewModel;
+        }
+    }
+}
