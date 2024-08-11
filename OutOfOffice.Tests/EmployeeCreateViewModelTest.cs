617923ba537d4ffe3a03b02f9769517abb4c40dd
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-11 15:21:31 +0200

    First integration test

--- OutOfOffice.Tests/EmployeeCreateViewModelTest.cs
+++ OutOfOffice.Tests/EmployeeCreateViewModelTest.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using OutOfOfficeEF;
 using OutOfOfficeWPF;
+using OutOfOfficeWPF.Services;
+using OutOfOfficeWPF.Stores;
 using OutOfOfficeWPF.ViewModels;
 
 namespace OutOfOfficeDomain.Tests
@@ -15,10 +19,33 @@ namespace OutOfOfficeDomain.Tests
         [Test]
         public void ExecuteSubmitCommand_WithValidEmployeeData_CreatesEmployee()
         {
-            var inMemoryContext = new InMemoryReservoomDbContextFactory().CreateDbContext();
-            //var employeeRepository = new SqlEmployeeRepository(inMemoryContext);
-            //var employeeService = new EmployeeService();
-            //var viewModel = new EmployeeCreateViewModel()
+            var dbContext = new InMemoryReservoomDbContextFactory().CreateDbContext();
+            dbContext.Database.Migrate();
+
+            var viewmodel = PrepareViewModel(dbContext);
+
+            viewmodel.FirstName = "Test1";
+            viewmodel.LastName = "Test2";
+
+            viewmodel.SubmitCommand.Execute(viewmodel);
+
+            Employee createdEmployee = dbContext.Employees.First();
+
+            Assert.That(createdEmployee, Is.Not.Null);
         }
+
+        private EmployeeCreateViewModel PrepareViewModel(OutOfOfficeDbContext dbContext)
+        {
+            var employeeRepository = new SqlEmployeeRepository(dbContext);
+            var employeeService = new EmployeeService(employeeRepository);
+            var navigationStore = new NavigationStore();
+            var authStore = new AuthStore();
+            var MakeHomeViewModel = () => new HomeViewModel(authStore);
+            var mockNavigationService = new NavigationService(navigationStore, MakeHomeViewModel);
+            var MakeNavigationBarViewModel = () => new NavigationBarViewModel(mockNavigationService, mockNavigationService, mockNavigationService, mockNavigationService, mockNavigationService, authStore);
+            var homeNavigationService = new LayoutNavigationService<HomeViewModel>(navigationStore, MakeHomeViewModel, MakeNavigationBarViewModel);
+            return new EmployeeCreateViewModel(employeeService, homeNavigationService); 
+        }
+
     }
 }
