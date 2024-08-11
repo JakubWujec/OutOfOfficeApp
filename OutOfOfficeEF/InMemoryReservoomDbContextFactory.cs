818c1bf61aae30ef818f5ae2b6d56f877511d8fd
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-11 14:10:59 +0200

    In memory db context

---
+++ OutOfOfficeEF/InMemoryReservoomDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOfficeEF
+{
+    public class InMemoryReservoomDbContextFactory : IOutOfOfficeDbContextFactory
+    {
+        private readonly SqliteConnection _connection;
+
+        public InMemoryReservoomDbContextFactory()
+        {
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+        }
+        public OutOfOfficeDbContext CreateDbContext()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite(_connection).Options;
+
+            return new OutOfOfficeDbContext(options);
+        }
+    }
+}
---
+++ OutOfOfficeEF/OutOfOfficeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OutOfOfficeDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOfficeEF
+{
+    public class OutOfOfficeDbContext: DbContext
+    {
+        public OutOfOfficeDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ApprovalRequest> ApprovalRequests { get; set; }
+        public bool IsNew<TEntity>(TEntity entity) where TEntity : class
+        {
+            return !this.Set<TEntity>().Local.Any(e => e == entity);
+        }
+    }
+}
