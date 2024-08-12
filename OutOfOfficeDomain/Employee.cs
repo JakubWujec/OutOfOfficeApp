170d52696196091d7ebd4d2684d07e48fceb0e3d
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-12 11:38:25 +0200

    Fetch user with role

--- OutOfOfficeDomain/Employee.cs
+++ OutOfOfficeDomain/Employee.cs
@@ -9,13 +9,16 @@ namespace OutOfOfficeDomain
         public string LastName { get; set; }
         public bool IsActive { get; set; }
         public int RoleId { get; set; } 
-
-        [ForeignKey("RoleId")]
-        public Role Role { get; set; }  
-
         public int OutOfOfficeBalance { get; set; }
         public ICollection<LeaveRequest> LeaveRequests { get; } = new List<LeaveRequest>();
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+
 
         public string FullName => $"{FirstName} {LastName}";
+        public override string ToString()
+        {
+            return $"{FullName} (${Role.Name})";
+        }
     }
 }
